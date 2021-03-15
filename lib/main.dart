295adb7d2fa0a48d65183fe78bf2b1528c3d633b
gbhb <5221abcd@gmail.com>
2021-03-15 18:16:23 +0800

    3

--- lib/main.dart
+++ lib/main.dart
@@ -3,38 +3,26 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:transparent_image/transparent_image.dart';
 import 'login/login_page.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
 
-final databaseReference = FirebaseDatabase.instance.reference();
-void createData(){
-  databaseReference.child("flutterDevsTeam1").set({
-    'name': 'Deepak Nishad',
-    'description': 'Team Lead'
-  });
-  databaseReference.child("flutterDevsTeam2").set({
-    'name': 'Yashwant Kumar',
-    'description': 'Senior Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam3").set({
-    'name': 'Akshay',
-    'description': 'Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam4").set({
-    'name': 'Aditya',
-    'description': 'Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam5").set({
-    'name': 'Shaiq',
-    'description': 'Associate Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam6").set({
-    'name': 'Mohit',
-    'description': 'Associate Software Engineer'
-  });
-  databaseReference.child("flutterDevsTeam7").set({
-    'name': 'Naveen',
-    'description': 'Associate Software Engineer'
-  });
-}
+final dbRef = FirebaseDatabase.instance.reference();
+
+// var v= await databaseReference.child('FirebaseIOT').once().then(DataSnapshot s{});
+
+// String humidity(){
+//   databaseReference.child('FirebaseIOT').once().then((DataSnapshot snapshot)  {
+//     print(snapshot.value['humidity'].toString());
+//     return snapshot.value['humidity'].toString();
+//   });
+// }
+// Future<String> humidity() async {
+//   String result = (await dbRef.instance.reference().child("FirebaseIOT/humidity").once()).value.toString();
+//   print(result);
+//
+//   return Future<String>.delayed(
+//       Duration(seconds: 2), () => result);;
+// }
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -49,7 +37,6 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(),
     );
   }
-
 }
 
 class MyHomePage extends StatefulWidget {
@@ -64,102 +51,223 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
   static const List<Widget> _widgetOption = <Widget>[
-    Icon(Icons.star, size: 100.0,),
-    Icon(Icons.mood_bad, size: 100.0,),
-    Icon(Icons.wb_sunny, size: 100.0,),
+    Icon(
+      Icons.star,
+      size: 100.0,
+    ),
+    Icon(
+      Icons.mood_bad,
+      size: 100.0,
+    ),
+    Icon(
+      Icons.wb_sunny,
+      size: 100.0,
+    ),
   ];
   SnackBar _snackBar1 = SnackBar(content: Text("You Click First Button"));
-
 
   void _onItemTap(int index) {
     setState(() => _selectedIndex = index);
   }
 
+  var Button = Row(
+    mainAxisSize: MainAxisSize.min,
+    children: [
+      ButtonTheme(
+        minWidth: 200.0,
+        height: 100,
+        child: RaisedButton(
+          onPressed: () {
+            dbRef.child("FirebaseIOT").update({
+              'led': '1',
+            });
+          },
+          child: Text('open'),
+        ),
+      ),
+      ButtonTheme(
+        minWidth: 200.0,
+        height: 100,
+        child: RaisedButton(
+          onPressed: () {
+            dbRef.child("FirebaseIOT").update({
+              'led': '0',
+            });
+          },
+          child: Text('close'),
+        ),
+      ),
+    ],
+  );
+  List<Map<dynamic, dynamic>> lists = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Scaffold example"),
-          leading: Icon(FontAwesomeIcons.database),
-        ),
-        bottomNavigationBar: BottomNavigationBar(items: [
+      appBar: AppBar(
+        title: Text("Scaffold example"),
+        leading: Icon(FontAwesomeIcons.database),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
           BottomNavigationBarItem(
-              icon: Icon(Icons.star,size: 20.0), title: Text('Star'),
+            icon: Icon(Icons.star, size: 20.0),
+            title: Text('Star'),
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.mood_bad,size: 20.0), title: Text('Sad')),
+              icon: Icon(Icons.mood_bad, size: 20.0), title: Text('Sad')),
           BottomNavigationBarItem(
-              icon: Icon(Icons.wb_sunny,size: 20.0), title: Text('Sunny')),
+              icon: Icon(Icons.wb_sunny, size: 20.0), title: Text('Sunny')),
         ],
-          onTap: _onItemTap,
-          currentIndex: _selectedIndex,),
-        drawer: Drawer(
-          child: ListView(
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                accountName: Text('oil'),
-                accountEmail: Text('userGmail@gmail.com'),
-                currentAccountPicture: Image.network('https://img.mttmp.com/images/2018/05/20/19/5741_12yBwE_d7k9lnm.jpg!r800x0.jpg'),
-                decoration: BoxDecoration(color: Colors.blueAccent),
-              ),
-              ListTile(
-                leading: Icon(Icons.favorite),
-                title: Text('Item1'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.headset),
-                title: Text('Item2'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.star),
-                title: Text('Item3'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
+        onTap: _onItemTap,
+        currentIndex: _selectedIndex,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('oil'),
+              accountEmail: Text('userGmail@gmail.com'),
+              currentAccountPicture: Image.network(
+                  'https://img.mttmp.com/images/2018/05/20/19/5741_12yBwE_d7k9lnm.jpg!r800x0.jpg'),
+              decoration: BoxDecoration(color: Colors.blueAccent),
+            ),
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text('Item1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.headset),
+              title: Text('Item2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text('Item3'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
-        body: Builder(
-          builder: (BuildContext context) {
+      ),
+      body: StreamBuilder(
+          stream: dbRef.onValue,
+          builder: (context, AsyncSnapshot<Event> snapshot) {
+            if (snapshot.hasData) {
+              lists.clear();
+              DataSnapshot dataValues = snapshot.data.snapshot;
+              Map<dynamic, dynamic> values = dataValues.value;
+              print(values["FirebaseIOT"].toString());
+              values.forEach((key, values) {
+                lists.add(values);
+              });
+              print(lists[0]["temperature"]);
+            }
             return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Flexible(
-                    child: _widgetOption.elementAt(_selectedIndex),
-                  ),
-                  Flexible(
-                    child: FadeInImage.assetNetwork(
-                        placeholder:'assets/1.jpg',
-                        image:'https://truth.bahamut.com.tw/s01/202101/5686a83fc52f34a606e2d5df43fe58f8.JPG'),
-                  ),
-                  Flexible(
-                    child: FadeInImage.memoryNetwork(
-                        placeholder: kTransparentImage,
-                        image:
-                        'https://titangene.github.io/images/cover/flutter.jpg'),
-                  ),
-                  ButtonTheme(
-                    minWidth: 200.0,
-                    height: 100,
-                    child: RaisedButton(
-                      onPressed: () {
-                        createData();
-                      },
-                      child: Text('create data'),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
-        ));
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          // Flexible(
+                          //   child: _widgetOption.elementAt(_selectedIndex),
+                          // ),
+                          Flexible(
+                            child: FadeInImage.assetNetwork(
+                                placeholder:'assets/1.jpg',
+                                image:'https://truth.bahamut.com.tw/s01/202101/5686a83fc52f34a606e2d5df43fe58f8.JPG'),
+                          ),
+                          Text(
+                            'temperature:'+lists[0]["temperature"].toString(),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: 'Roboto',
+                              letterSpacing: 0.5,
+                              fontSize: 20,
+                            ),
+                          ),Text(
+                            'led:'+lists[0]["led"].toString(),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: 'Roboto',
+                              letterSpacing: 0.5,
+                              fontSize: 20,
+                            ),
+                          ),Text(
+                            'humidity:'+lists[0]["humidity"].toString(),
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w800,
+                              fontFamily: 'Roboto',
+                              letterSpacing: 0.5,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Button,
+                        ],
+                      ),
+                    );
+          }),
+      // Builder(
+      //   builder: (BuildContext context) {
+      //     return Center(
+      //       child: Column(
+      //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //         children: <Widget>[
+      //           // Flexible(
+      //           //   child: _widgetOption.elementAt(_selectedIndex),
+      //           // ),
+      //           Flexible(
+      //             child: FadeInImage.assetNetwork(
+      //                 placeholder:'assets/1.jpg',
+      //                 image:'https://truth.bahamut.com.tw/s01/202101/5686a83fc52f34a606e2d5df43fe58f8.JPG'),
+      //           ),
+      //           Text(
+      //             humidity().timeout(Duration(seconds: 5)).toString(),
+      //             style: TextStyle(
+      //               color: Colors.black,
+      //               fontWeight: FontWeight.w800,
+      //               fontFamily: 'Roboto',
+      //               letterSpacing: 0.5,
+      //               fontSize: 20,
+      //             ),
+      //           ),
+      //           Button,
+      //         ],
+      //       ),
+      //     );
+      //   },
+      // ),
+      // FutureBuilder(
+      // future: dbRef.child("FirebaseIOT").once(),
+      // builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+      //   if (snapshot.hasData) {
+      //     String humidity = snapshot.data.value['humidity'].toString();
+      //     String led = snapshot.data.value['led'].toString();
+      //     String temperature = snapshot.data.value['temperature'].toString();
+      //     return new ListView.builder(
+      //         shrinkWrap: true,
+      //         itemCount: lists.length,
+      //         itemBuilder: (BuildContext context, int index) {
+      //           return Card(
+      //             child: Column(
+      //               crossAxisAlignment: CrossAxisAlignment.start,
+      //               children: <Widget>[
+      //                 Text("humidity: " + humidity),
+      //                 Text("led: "+ led),
+      //                 Text("temperature: " +temperature),
+      //               ],
+      //             ),
+      //           );
+      //         });
+      //   }
+      //   return CircularProgressIndicator();
+      // }),
+    );
   }
 }
